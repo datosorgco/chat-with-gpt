ceb7e58bc2243d379f1fb691f57563534d9c7a2d
Julian Olarte <38110237+pollitoiracundo@users.noreply.github.com>
2023-08-08 16:43:42 -0500

    start

---
+++ app/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Detener el contenedor actual 
+CONTAINER_ID=$(docker ps | grep openchat | awk '{print $1}')
+docker stop $CONTAINER_ID
+
+# Actualizar el código fuente
+git pull
+
+# Reconstruir la imagen
+docker build -t openchat .
+
+# Ejecutar nuevo contenedor 
+docker run -v $(pwd)/data:/app/data -p 8081:3000 openchat:latest
